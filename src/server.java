29e93ea6bbe5c45e46b004e503c76133c16c5c48
Drew <trix.krypt15@gmail.com>
2019-01-18 16:06:40 -0500

    Added all of the server code needed to make it functional, but I don't know if it works.

--- src/server.java
+++ src/server.java
@@ -1,11 +1,10 @@
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+import java.util.StringTokenizer;
 
-public class server {
+public class server implements Runnable{
 
     private static ServerSocket host;
     private static Socket client;
@@ -28,11 +27,118 @@ public class server {
     }
     //@Override
     public void run(){
-        BufferedReader in;
-        PrintWriter out;
-        BufferedOutputStream dataOut;
-        String fileRequested;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        BufferedOutputStream dataOut = null;
+        String fileRequested = null;
+        try{
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintWriter(client.getOutputStream());
+            dataOut = new BufferedOutputStream(client.getOutputStream());
+            String input = in.readLine();
+            StringTokenizer parse = new StringTokenizer(input);
+            String method = parse.nextToken().toUpperCase();
+            fileRequested = parse.nextToken().toLowerCase();
+            if (!method.equals("GET")  &&  !method.equals("HEAD")) {
+                File file = new File(new File("."), "not_supported.html");
+                int fileLength = (int) file.length();
+                String contentMimeType = "text/html";
+                byte[] fileData = readFileData(file, fileLength);out.println("HTTP/1.1 501 Not Implemented");
 
+                out.println("Server: Java HTTP Server from SSaurel : 1.0");
+                out.println("Date: " + new Date());
+                out.println("Content-type: " + contentMimeType);
+                out.println("Content-length: " + fileLength);
+                out.println();
+                out.flush();
 
+                dataOut.write(fileData, 0, fileLength);
+                dataOut.flush();
+
+            } else {
+                // GET or HEAD method
+                if (fileRequested.endsWith("/")) {
+                    fileRequested += app.getDefaultFile(); //WRITE THIS METHOD
+                }
+
+                File file = new File(new File("."), fileRequested);
+                int fileLength = (int) file.length();
+                String content = getContentType(fileRequested);
+
+                if (method.equals("GET")) { // GET method so we return content
+                    byte[] fileData = readFileData(file, fileLength);
+
+                    // send HTTP Headers
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Server: EasyServer : 1.0");
+                    out.println("Date: " + new Date());
+                    out.println("Content-type: " + content);
+                    out.println("Content-length: " + fileLength);
+                    out.println(); // blank line between headers and content, very important !
+                    out.flush(); // flush character output stream buffer
+
+                    dataOut.write(fileData, 0, fileLength);
+                    dataOut.flush();
+                }
+            }
+        } catch (FileNotFoundException fnfe) {
+            try {
+                fileNotFound(out, dataOut, fileRequested);
+            } catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+
+        } catch (IOException ioe) {
+            System.err.println("Server error : " + ioe);
+        } finally {
+            try {
+                in.close();
+                out.close();
+                dataOut.close();
+                client.close(); // we close socket connection
+            } catch (Exception e) {
+                System.err.println("Error closing stream : " + e.getMessage());
+            }
+        }
+    }
+    private byte[] readFileData(File file, int fileLength) throws IOException {
+        FileInputStream fileIn = null;
+        byte[] fileData = new byte[fileLength];
+
+        try {
+            fileIn = new FileInputStream(file);
+            fileIn.read(fileData);
+        } finally {
+            if (fileIn != null)
+                fileIn.close();
+        }
+
+        return fileData;
+    }
+
+    // return supported MIME Types
+    private String getContentType(String fileRequested) {
+        if (fileRequested.endsWith(".htm")  ||  fileRequested.endsWith(".html"))
+            return "text/html";
+        else
+            return "text/plain";
+    }
+
+    private void fileNotFound(PrintWriter out, OutputStream dataOut, String fileRequested) throws IOException {
+        File file = new File(new File("."), "404.html");
+        int fileLength = (int) file.length();
+        String content = "text/html";
+        byte[] fileData = readFileData(file, fileLength);
+
+        out.println("HTTP/1.1 404 File Not Found");
+        out.println("Server: Java HTTP Server from SSaurel : 1.0");
+        out.println("Date: " + new Date());
+        out.println("Content-type: " + content);
+        out.println("Content-length: " + fileLength);
+        out.println(); // blank line between headers and content, very important !
+        out.flush(); // flush character output stream buffer
+
+        dataOut.write(fileData, 0, fileLength);
+        dataOut.flush();
     }
 }
