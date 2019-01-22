54bed7c1639e13961554f749732ffc4b4f4785ce
Drew <trix.krypt15@gmail.com>
2019-01-21 21:06:47 -0500

    Application is functional, though there is a bug where if the user accesses the server then requests an invalid page (getting the 404 page) the server window can't be closed by clicking X at the top right.

--- src/app.java
+++ src/app.java
@@ -6,20 +6,47 @@ import java.awt.event.ActionListener;
 public class app {
 
     private static JFrame frame = new JFrame("EasyServer");
+    private static Boolean serverIsOn = false;
+    private static String folder = ".";
 
     public static void main(String[] args){
         buildFrame();
     }
 
     private static void buildFrame(){
-        frame.setPreferredSize(new Dimension(600,800));
-        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(300,350));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JButton startButton = new JButton("Start/Stop Server");
+        frame.getContentPane().add(new JLabel("<html>Welcome to EasyServer! Simply find the folder containing your website's files by clicking the button below, then click the 'Start Server' button at the bottom. To shut down the server, close this window.<br/><br/>NOTE: Make sure you have files in your folder named 'index.html' (your website's home page), '404.html' (for when someone attempts to find a file on your website that doesn't exist), and 'not_supported.html' (for when someone tries to interact with the website in a way that EasyServer doesn't support). <br/><html>"), BorderLayout.BEFORE_FIRST_LINE);
+
+
+        JButton findButton = new JButton("Select Server Files");
+        findButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                JFileChooser chooser = new JFileChooser();
+                chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);chooser.setAcceptAllFileFilterUsed(false);
+                if (chooser.showOpenDialog(findButton) == JFileChooser.APPROVE_OPTION) {
+                    setFolder(chooser.getSelectedFile().toString());
+                }
+                else {
+                    System.out.println("No Selection ");
+                }
+            }
+        });
+        frame.getContentPane().add(findButton, BorderLayout.CENTER);
+
+        JButton startButton = new JButton("Start Server");
         startButton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
-                server.main();
+                if(!serverIsOn) {
+                    server.main(folder);
+                    serverIsOn = !serverIsOn;
+                }else{
+                    server.stop();
+                    serverIsOn = !serverIsOn;
+                }
             }
         });
         frame.getContentPane().add(startButton, BorderLayout.SOUTH);
@@ -28,7 +55,11 @@ public class app {
         frame.setVisible(true);
     }
 
-    public static String getDefaultFile(){
+    static String getDefaultFile(){
         return "index.html";
+    }
+
+    private static void setFolder(String folder1){
+        folder = folder1;
     }
 }
--- src/server.java
+++ src/server.java
@@ -1,25 +1,32 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.StringTokenizer;
 
 public class server implements Runnable{
+    private static String folder;
+    private Socket client;
+    private static ArrayList<Thread> activeThreads = new ArrayList<>();
+    //private static Thread t;
 
-    private static ServerSocket host;
-    private static Socket client;
-
-    public server(){
-        Boolean init = true;
+    private server(Socket s){
+        client = s;
     }
-    public static void main(){
+    static void main(String f){
+        folder = f;
         try{
-            host = new ServerSocket(80);
-            client = host.accept();
+            ServerSocket host = new ServerSocket(8080);
+            //client = host.accept();
 
             while(true){
-                server server1 = new server();
-                new Thread(server1).start();
+                server server1 = new server(host.accept());
+                //t = new Thread(server1);
+                //t.start();
+                Thread t = new Thread(server1);
+                t.start();
+                activeThreads.add(t);
             }
         }catch(IOException e){
             System.err.println("Server error (IOException, line 25): " + e.getMessage());
@@ -39,12 +46,12 @@ public class server implements Runnable{
             String method = parse.nextToken().toUpperCase();
             fileRequested = parse.nextToken().toLowerCase();
             if (!method.equals("GET")  &&  !method.equals("HEAD")) {
-                File file = new File(new File("."), "not_supported.html");
+                File file = new File(new File(folder), "not_supported.html");
                 int fileLength = (int) file.length();
                 String contentMimeType = "text/html";
                 byte[] fileData = readFileData(file, fileLength);out.println("HTTP/1.1 501 Not Implemented");
 
-                out.println("Server: Java HTTP Server from SSaurel : 1.0");
+                out.println("Server: EasyServer : 1.0");
                 out.println("Date: " + new Date());
                 out.println("Content-type: " + contentMimeType);
                 out.println("Content-length: " + fileLength);
@@ -60,7 +67,7 @@ public class server implements Runnable{
                     fileRequested += app.getDefaultFile();
                 }
 
-                File file = new File(new File("."), fileRequested);
+                File file = new File(new File(folder), fileRequested);
                 int fileLength = (int) file.length();
                 String content = getContentType(fileRequested);
 
@@ -124,7 +131,7 @@ public class server implements Runnable{
     }
 
     private void fileNotFound(PrintWriter out, OutputStream dataOut, String fileRequested) throws IOException{
-        File file = new File(new File("."), "404.html");
+        File file = new File(new File(folder), "404.html");
         int fileLength = (int) file.length();
         String content = "text/html";
         byte[] fileData = readFileData(file, fileLength);
@@ -139,5 +146,11 @@ public class server implements Runnable{
 
         dataOut.write(fileData, 0, fileLength);
         dataOut.flush();
+    }
+    @SuppressWarnings("deprecation")
+    static void stop(){
+        for(Thread i : activeThreads){
+            i.stop();
+        }
     }
 }
